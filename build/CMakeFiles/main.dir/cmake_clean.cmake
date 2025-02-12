7505203fa5b8aa8fb16bdde02fdcce311a4ffa1a
zyx <2511305125@qq.com>
2025-02-12 14:14:53 +0000

    Test

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/Test.cpp.o"
+  "CMakeFiles/main.dir/Test.cpp.o.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "CMakeFiles/main.dir/motor.cpp.o"
+  "CMakeFiles/main.dir/motor.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
